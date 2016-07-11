00c80009e77e43570339dc2f61ebff7605bbf486
Alexpug <alexandre.luu@epitech.eu>
2016-07-11 14:06:35 +0200

    Add length entries git

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@ extern crate git2;
 
 use std::process::Command;
 use git2::Repository;
+use git2::StatusOptions;
 
 fn main(){
     // Call ls -lah --color and catch the output
     let output = Command::new("ls").arg("-lah").arg("--color").output()
         .expect("Failed to execute process");
-    let repository = match Repository::init(".") {
+    let repository = match Repository::open(".") {
         Ok(repository)  => repository,
         Err(e)          => panic!("Failed to init: {}", e),
     };
+    //let mut status_option: Option<git2::StatusOptions> = StatusOptions::new();
+    //let mut statuses = repository.statuses(&mut status_option);
+    let index_repo = match repository.index(){
+        Ok(Index)       => Index,
+        Err(e)          => panic!("Failed to get index {}", e),
+    };
+    println!("length : {}", index_repo.len());
+    println!("------------------------------");
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
